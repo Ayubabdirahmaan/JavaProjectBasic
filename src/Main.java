359a73e4446c730ecaeb2e8ed637cbb28803e4fa
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-27 09:44:15 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        bithday
-//        Scanner birthDay = new Scanner(System.in);
-//        System.out.println("Enter year of birthDay:");
-//        int year = birthDay.nextInt();
-//        int limit = 2026;
-//       int result = limit - year;
-//        System.out.println("Your age is:" + result + " Years");
+//        birth day
+        Scanner bithDay = new Scanner(System.in);
+        System.out.println("Enter your bithday");
+        int day = 2026;
+        int sources = bithDay.nextInt();
+        int result = day - sources;
+        System.out.println("Your age is:" + result );
 
-// add total subjects and avarage
-//                Scanner total = new Scanner(System.in);
-//                System.out.println("Enter subject 1 marks");
-//                int SubjectOne = total.nextInt();
-//                System.out.println("Enter subject 2 marks");
-//                 int subjectTwo = total.nextInt();
-//        System.out.println("Enter subject 3 marks");
-//        int subjectThree = total.nextInt();
-//        int totalSubject = SubjectOne + subjectTwo + subjectThree;
-//        double  resultSubject = totalSubject / 3.0;
-//        System.out.println("Total Subject is: " + totalSubject);
-//        System.out.println("Average subject is: " + resultSubject);
+//        calaculate your subjects
+        Scanner subjectOne = new Scanner(System.in);
 
-    
-
+        System.out.println("Enter subject 1:");
+        int subOne = subjectOne.nextInt();
+        Scanner subjectTwo = new Scanner(System.in);
+        System.out.println("Enter subject 2: ");
+        int subTwo = subjectTwo.nextInt();
+        Scanner subjectThree = new Scanner(System.in);
+        System.out.println("Enter subject 3: ");
+        int subThree = subjectThree.nextInt();
+        int total = subOne + subTwo + subThree;
+        double average = (double) total /3;
+        System.out.println("Total of Subjects : " + total);
+        System.out.println("Average of Subjects:" + average);
+    }
 }
