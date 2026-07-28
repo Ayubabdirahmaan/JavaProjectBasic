ab3c81b7c37f61912c4ccf20ce221f6985585a72
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-28 11:11:00 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        birth day
-        String name = "Ayubdheere";
-        System.out.println(name);
-        String myNames;
-        myNames = "abdiwali";
-        int current = 90;
-        current = 99;
-        System.out.println(current);
-
-        int age = 10;
-        if(age >= 18) {
-            System.out.println("your an adult");
-        }else if(age >= 13) {
-            System.out.println("Your are teeneeger");
-        }else{
-            System.out.println("you are child");
-        }
-
+    int age = 90;
+    age = 399+1;
+        System.out.println(age);
 
     }
 }
