4bd94e4073dfba1ea038934d90942ccb2e464d7f
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-28 16:19:00 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-    int age = 90;
-    age = 399+1;
-        System.out.println(age);
+//
+        String name = "Ayubdheere";
+        int age = 26;
+        double height = 1.8;
+        boolean isLearningJava = true;
+        System.out.println("My name is: "+ name + age + height + isLearningJava);
 
+        int numberOne = 100;
+        int numberTwo = 100;
+        int add = numberOne + numberTwo;
+        int sub = numberOne - numberTwo;
+        int mult = numberOne * numberTwo;
+        int div = numberOne / numberTwo;
+        int module = numberOne % numberTwo;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+        System.out.println(module);
+        String nameone = "Ayub Abdi";
+        int ageone = 25;
+        double heightone = 1.9;
+        boolean isStudents = true;
+        System.out.println("My name is " + nameone + " and my age: " + ageone +
+                " my height: " + heightone + " Im student " + isStudents);
     }
 }
