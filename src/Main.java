ab427be360e162dfc67b53585e6b72ac9c164646
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-29 13:14:18 +0300

    Added

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 //
-        String name = "Ayubdheere";
-        int age = 26;
-        double height = 1.8;
-        boolean isLearningJava = true;
-        System.out.println("My name is: "+ name + age + height + isLearningJava);
-
-        int numberOne = 100;
-        int numberTwo = 100;
-        int add = numberOne + numberTwo;
-        int sub = numberOne - numberTwo;
-        int mult = numberOne * numberTwo;
-        int div = numberOne / numberTwo;
-        int module = numberOne % numberTwo;
-        System.out.println(add);
-        System.out.println(sub);
-        System.out.println(mult);
-        System.out.println(div);
-        System.out.println(module);
-        String nameone = "Ayub Abdi";
-        int ageone = 25;
-        double heightone = 1.9;
-        boolean isStudents = true;
-        System.out.println("My name is " + nameone + " and my age: " + ageone +
-                " my height: " + heightone + " Im student " + isStudents);
+       int num1 = 100;
+        System.out.println(num1);
     }
 }
