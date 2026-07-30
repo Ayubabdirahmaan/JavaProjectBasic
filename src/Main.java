f04f6a7a943e8871525780ec6ceb77d8a5270cee
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-30 20:54:48 +0300

    feat: add java in methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-//
-       int num1 = 100;
-        System.out.println(num1);
+        NewObject1 obj = new NewObject1();
+        obj.NewObject1();
+        NewObject1 dis = new NewObject1();
+        dis.Display();
     }
 }
