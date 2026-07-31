30be79ebb7257f3e0b1bf70959cff5b6765b9743
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-31 19:32:26 +0300

    Added

--- src/DisplayTest.java
+++ src/DisplayTest.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class DisplayTest {
     public void DisplayOne() {
         Scanner test = new Scanner(System.in);
-        System.out.println("Enter You name");
-        String FullName = test.next();
-        System.out.println("Enter Your Age");
-        int age = test.nextInt();
-        System.out.println("My Name: " + FullName + " And My Age: "+ age);
+        System.out.println("Enter number 1");
+        int number1 = test.nextInt();
+        System.out.println("Enter number 2");
+        int number2 = test.nextInt();
+
+        int sum = number1 + number2;
+        System.out.println("Total is:" + sum);
     }
 }
