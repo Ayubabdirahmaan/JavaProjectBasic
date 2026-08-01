fa36bf84c95971aa43357b848dbab64106d5542b
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-01 15:44:35 +0300

    added

--- src/publics.java
+++ src/publics.java
@@ -5,9 +5,11 @@ public class publics {
     String name1,name2,name3;
     public  void publics() {
 
-        System.out.println("Enter you name");
+        System.out.println("Enter you name one");
         name1 = info.nextLine();
+        System.out.println("Enter you name two");
         name2 = info.nextLine();
+        System.out.println("Enter you name three");
         name3 = info.nextLine();
 
         System.out.println("My full name is: " + name1 + name2 + name3);
