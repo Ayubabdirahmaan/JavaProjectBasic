70122038b13d7803483e5867eab419367b87838c
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-02 09:57:30 +0300

    added

---
+++ src/Example3.java
@@ -0,0 +1,11 @@
+public class Example3 {
+    String sayHi;
+    String name;
+    public  void inputTest () {
+        sayHi = "Hello Welcome";
+        name = "Ayub Abdi";
+    }
+    public  void displayTest() {
+        System.out.println(sayHi  + " " + name);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ public class Main {
         Example1 obj1 = new Example1();
             obj1.input();
             obj1.Display();
+            
             Example2 obj2 = new Example2();
             obj2.inputUser();
             obj2.displayUser();
+
+            Example3 obj3 = new Example3();
+            obj3.inputTest();
+            obj3.displayTest();
     }
 }
