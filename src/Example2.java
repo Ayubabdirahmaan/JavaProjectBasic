4e64511da3bf634176631a91d0f16fea0edb55b2
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-04 17:07:42 +0300

    Added

--- src/Example2.java
+++ src/Example2.java
@@ -1,22 +1,15 @@
 import java.util.Scanner;
 
 public class Example2 {
-    Scanner subjects = new Scanner(System.in);
-    int subject1,subject2,subject3;
-    int sum;
-    double average;
-    public  void inputUser() {
-        System.out.println("Enter Subject one: ");
-        subject1 = subjects.nextInt();
-        System.out.println("Enter Subject two: ");
-        subject2 = subjects.nextInt();
-        System.out.println("Enter subject three: ");
-        subject3 = subjects.nextInt();
-        sum = subject1 + subject2 + subject3;
-        average = sum / 3;
-    }
-    public  void  displayUser() {
-        System.out.println("Total of subjects is: " + sum);
-        System.out.println("Average of Subjects: " + average);
+    Scanner kb = new Scanner(System.in);
+    public Example2() {
+        System.out.println("Enter number: ");
+       int  n = kb.nextInt();
+       if(n > 100) {
+           System.out.println(n + " is greater then 100");
+       } else if(n < 100) {
+           System.out.println(n + " is less then 100");
+       }else
+           System.out.println(n + " is 100");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Example1 obj1 = new Example1();
-//            obj1.input();
-//            obj1.Display();
-//
-//            Example2 obj2 = new Example2();
-//            obj2.inputUser();
-//            obj2.displayUser();
-//
-//            Example3 obj3 = new Example3();
-//            obj3.inputTest();
-//            obj3.displayTest();
-//
-//            Example4 obj4 = new Example4();
-//            obj4.testInput();
-//            obj4.testDisplay();
+        new Example2();
 
-//            Example5 obj5 = new Example5();
-//            obj5.calculateInput();
-//            obj5.calculateDisplay();
-//
-//            Example6 obj6 = new Example6();
-//            obj6.inputExample();
-//            obj6.displayExample();
 
-            Example7 obj6 = new Example7();
-            obj6.InputLanguages();
-            obj6.secondInput();
-            obj6.displayLanguage();
-            obj6.lastPoint();
     }
 }
